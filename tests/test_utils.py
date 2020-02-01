2c9b77ae5183a85a199dd5249313a089fab00d42
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2020-02-01 18:49:54 -0300

    Added black in project

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,12 +4,11 @@ from musixmatch import _set_page_size
 
 
 class TestUtils(unittest.TestCase):
-
     def test_set_page_size(self):
         self.assertEqual(_set_page_size(200), 100)
         self.assertEqual(_set_page_size(-10), 1)
         self.assertEqual(_set_page_size(10), 10)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
