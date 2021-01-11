33f9c9a0654eccdc3359412e6f1e132f138ea8ee
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2021-01-10 21:40:59 -0300

    Refactory method track_lyrics_get

--- tests/results.py
+++ tests/results.py
@@ -51,3 +51,25 @@ TRACKS = {
         },
     }
 }
+
+
+TRACK_LYRICS = {
+    "message": {
+        "header": {"status_code": 200, "execute_time": 0.04367995262146},
+        "body": {
+            "lyrics": {
+                "lyrics_id": 7260188,
+                "restricted": 0,
+                "instrumental": 0,
+                "lyrics_body": "Now and then I think of when we were together\r\n...",
+                "lyrics_language": "en",
+                "script_tracking_url": "http:\/\/tracking.musixmatch.com\/t1.0\/m42By\/J7rv9z",
+                "pixel_tracking_url": "http:\/\/tracking.musixmatch.com\/t1.0\/m42By\/J7rv9z6q9he7AA",
+                "lyrics_copyright": "Lyrics powered by www.musiXmatch.com",
+                "backlink_url:"
+                "https://www.musixmatch.com/lyrics/Gotye-feat-Kimbra/Somebody-That-I-Used-to-Know"
+                "updated_time": "2012-04-26T02:09:39Z",
+            }
+        },
+    }
+}
